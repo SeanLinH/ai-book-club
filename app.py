b531effa7f3f24848b97a13d7fc2dc1fc7096b10
Ubuntu <ubuntu@ip-172-31-83-114.ec2.internal>
2024-03-16 08:47:17 +0000

    update

--- app.py
+++ app.py
@@ -7,10 +7,38 @@ import uuid
 st.session_state['show_user_form'] = True
 st.session_state['username'] = ""
 
+# def login(user, password):
+#     """簡單的登入驗證"""
+#     return user in users_db and users_db[user]['password'] == password
+
+# def user_form(user):
+#     """顯示用戶資料表單"""
+#     with st.form("user_info"):
+#         st.write(f"歡迎, {user}! 請填寫以下資訊:")
+#         interest = st.text_input("你的興趣是什麼？", key="interest")
+#         profession = st.text_input("你的職業是什麼？", key="profession")
+#         role = st.selectbox("你在學習中扮演的角色是什麼？", ["學生", "教師", "研究者", "業餘愛好者"], key="role")
+        
+#         # 當用戶提交表單時，更新用戶資料
+#         submitted = st.form_submit_button("提交")
+#         if submitted:
+#             users_db[user].update({"interest": interest, "profession": profession, "role": role})
+#             st.success("資料已更新")
 
 
 # 如果show_user_form狀態為True，則在側邊欄顯示表單
 if st.session_state.get('show_user_form', False):
+    # with st.sidebar:
+    # st.title("用戶登入")
+    # user = st.text_input("用戶名稱", key="user")
+    # password = st.text_input("密碼", type="password", key="password")
+    # if st.button("登入"):
+    #     if login(user, password):
+    #         st.session_state['user'] = user
+    #         st.success(f"歡迎回來, {user}!")
+    #     else:
+    #         st.error("登入失敗，請檢查用戶名稱和密碼")
+    
     with st.sidebar.form(key='user_info_form'):
         st.write("用戶資料表單")
         title = st.text_input("讀書會主題", placeholder='大型語言模型讀書會')
